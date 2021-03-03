6ce5d650e652e89667ce08d9a971117fa34b0fc0
Adrian Inza-Cruz <ainzacruz@gmail.com>
2021-03-03 11:12:46 -0800

    Webpack config with typescript/react/node > > Co-authored-by: Adrian Inza-Cruz ainzacruz@gmail.com Co-authored-by: Kim Chiu kimchiux123@gmail.com Co-authored-by: Phillip Bannister phillip.kbannister@gmail.com Co-authored-by: Tammy Le le.tammy979@gmail.com Co-authored-by: Lucas Mobley lucas@lucasmobley.com

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        WORKING
+      </div>
+    );
+  }
+}
+
+export default App;
