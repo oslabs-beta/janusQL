a70e80302ade3f53024eb48ff9952f8e1d0435d8
Phillip Kekoa Bannister <phillip.kbannister@gmail.com>
2021-03-05 20:00:24 -0800

    added eslint dependency and linted files

--- electron/main.ts
+++ electron/main.ts
@@ -1,6 +1,6 @@
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
-import * as url from "url";
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import * as url from 'url';
 
 let mainWindow: Electron.BrowserWindow | null;
 
@@ -14,23 +14,23 @@ function createWindow() {
     },
   });
 
-  if (process.env.NODE_ENV === "development") {
-    mainWindow.loadURL(`http://localhost:4000`);
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:4000');
     mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadURL(
       url.format({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file:",
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
         slashes: true,
       })
     );
   }
 
-  mainWindow.on("closed", () => {
+  mainWindow.on('closed', () => {
     mainWindow = null;
   });
 }
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 app.allowRendererProcessReuse = true;
