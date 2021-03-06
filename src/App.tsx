eba055f09724180476c0b64352784f4529e73cd6
Lucas Mobley <Lucas@LucasMobley.com>
2021-03-06 08:49:26 -0800

    login page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Homepage from './Homepage';
 import Login from './components/Login'
-import Signup from './components/Signup'
+// import Signup from './components/Signup'
 import Main from './Main';
 
 const mainElement = document.createElement('div');
