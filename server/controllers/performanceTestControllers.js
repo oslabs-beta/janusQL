93ba902de5b3ce66512429de5c2dc7a33d84cfce
Kim Chiu <kimchiux123@gmail.com>
2021-03-07 17:06:52 -0800

    response time test is working as expected
    Co-authored-by: Adrian Inza-Cruz ainzacruz@gmail.com
    Co-authored-by: Phillip Bannister phillip.kbannister@gmail.com
    Co-authored-by: Tammy Le le.tammy979@gmail.com
    Co-authored-by: Lucas Mobley lucas@lucasmobley.com

--- server/controllers/performanceTestControllers.js
+++ server/controllers/performanceTestControllers.js
@@ -10,99 +10,36 @@ performanceTestControllers.responseTime = ((req, res, next) => {
   // SCRUB INPUTS RES.BODY
   
   // we are defining the query here for testing sake, user's will provide us the query in the electron
-  // const queryTester = `query {
-  //   country(code: "BR") {
-  //     name
-  //     native
-  //     capital
-  //     emoji
-  //     currency
-  //     languages {
-  //       code
-  //       name
-  //     }
-  //   }
-  // }`;
-  // var myHeaders = {
-  //   "Content-Type": "application/json"
-  // }
-
-  //start timer
-  // const start = Date.now();
-  // console.log('start: ', start);
-
-  // var graphql = JSON.stringify({
-  //   query: `query {
-  //     country(code: "BR") {
-  //       name
-  //       native
-  //       capital
-  //       emoji
-  //       currency
-  //       languages {
-  //         code
-  //         name
-  //       }
-  //     }
-  //   }`
-  //   // variables: {}
-  // })
-
-//   var requestOptions = {
-//     method: 'POST',
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     body: graphql,
-//     // redirect: 'follow'
-//   };
-
-//   fetch("https://countries.trevorblades.com/", requestOptions)
-//     .then(response => response.text())
-//     .then(result => console.log(result))
-//     .then(() => {
-//       const end = Date.now();
-//       console.log('end: ', end);
-//       const duration = end - start;
-//       console.log('duration:', duration);
-//       res.locals.responseTime = duration;
-//       return next();
-//     })
-//     .catch(error => console.log('error', error));
-// });
-
-
+  const queryTester = `query {
+    country(code: "BR") {
+      name
+      native
+      capital
+      emoji
+      currency
+      languages {
+        code
+        name
+      }
+    }
+  }`;
 
   //start timer
   const start = Date.now();
   console.log('start: ', start);
-  // replace spacex url with url;
+
+  // replace countries url with users url;
   fetch('http://countries.trevorblades.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      query: `
-        query {
-          country(code: "BR") {
-            name
-            native
-            capital
-            emoji
-            currency
-            languages {
-              code
-              name
-            }
-          }
-        }`
+      query: queryTester
     })
   })
     .then(res => {
-      res.text();
-      // console.log('r.json', res);
-      // return res;
+      return res.text();
     })
     .then(data => {
       console.log('data returned:', data)
@@ -115,9 +52,7 @@ performanceTestControllers.responseTime = ((req, res, next) => {
       res.locals.responseTime = duration;
       return next();
     })
-    .catch( err => console.log('error', err)); // insert global error handler
+    .catch(err => console.log('error', err)); // insert global error handler
 });
 
 module.exports = performanceTestControllers;
-
-// req();
