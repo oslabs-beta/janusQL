388d11ac2ff06e130bcc7f858099ef3bdec5b234
Adrian Inza-Cruz <ainzacruz@gmail.com>
2021-03-07 22:04:14 -0800

    added testing buttons and added material UI palette MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Main from './Main';
 import './styles/styles.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 const mainElement = document.createElement('div');
 document.body.appendChild(mainElement);
+
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: '#FF8E53'
+    },
+    primary: {
+      main: '#FE688B'
+    }
+  }
+})
 
 const App = () => {
   return (
--- src/components/QueryPlayground.tsx
+++ src/components/QueryPlayground.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import '../../node_modules/codemirror/lib/codemirror.css';
-import '../../node_modules/codemirror/theme/material.css';
+import '../../node_modules/codemirror/theme/monokai.css';
 import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/edit/closebrackets';
 import { UnControlled as CodeMirror } from 'react-codemirror2';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 
 
 const QueryPlayground = (props:any) => {
@@ -15,18 +19,26 @@ const QueryPlayground = (props:any) => {
   
   return (
     <div className="playground">
-      <div className="playground-title">Query</div>
+      <Container maxWidth='sm'>
+      <h1 className="playground-title">Query</h1>
       <CodeMirror
         onBeforeChange = {handleChange} 
         value={value}
         className="code-mirror"
         options={{
+          autoCloseBrackets: true,
+          tabSize: 2,
           lineWrapping: true,
           lint: true,
-          mode: "javascript",
-          lineNumbers: true,
+          mode: 'javascript',
+          lineNumbers: true
         }}
       />
+      <Box display="flex" justifyContent="space-evenly" mt="1em">
+        <Button variant="contained" color='primary'>Load Test</Button>
+        <Button variant="contained" color='secondary'>Security Test</Button>
+      </Box>
+      </Container>
     </div>
   );
 };
