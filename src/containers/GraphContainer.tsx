c7e04ff84997159ae6d92072461ddd2f307e4381
Adrian Inza-Cruz <ainzacruz@gmail.com>
2021-03-08 00:30:45 -0800

    added chartjs and dummy graphs

--- src/containers/GraphContainer.tsx
+++ src/containers/GraphContainer.tsx
@@ -3,6 +3,9 @@ import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Graph from '../components/Graph';
 
 const GraphContainer = () => {
   return (
@@ -13,6 +16,39 @@ const GraphContainer = () => {
           <TextField fullWidth variant='outlined' color='secondary' label='Enter GraphQL Endpoint'></TextField>
           <Button className='submit-btn' variant='contained' color='secondary'>Submit</Button>
         </Box>
+
+        <Grid className='grid' container spacing={1} justify='center'>
+          <Box display='flex' width='100%' justifyContent='space-between' marginTop={5}>
+            <Grid item>
+              <Paper style={{height:400, width:500}}>
+                <Graph className='graph'/>
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper style={{height:400, width:500}}>
+              <Graph className='graph'/>
+              </Paper>
+            </Grid>
+          </Box>
+          <Box display='flex' width='100%' justifyContent='space-between' marginTop={5}>
+            <Grid item>
+              <Paper style={{height:200, width:500}}>
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper style={{height:200, width:500}}></Paper>
+            </Grid>
+          </Box>
+          <Box display='flex' width='100%' justifyContent='space-between' marginTop={5}>
+            <Grid item>
+              <Paper style={{height:400, width:500}}></Paper>
+            </Grid>
+            <Grid item>
+              <Paper style={{height:400, width:500}}></Paper>
+            </Grid>
+          </Box>
+        </Grid>
+
       </Container>
     </div>
   );
