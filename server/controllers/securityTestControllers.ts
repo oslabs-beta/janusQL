6e4e4207f8b972b303b8077e8fe3384ecb9ecb26
Phillip Kekoa Bannister <phillip.kbannister@gmail.com>
2021-03-09 09:43:22 -0800

    add dos test to securityTestControllers
    Co-authored-by: Adrian Inza-Cruz ainzacruz@gmail.com
    Co-authored-by: Kim Chiu kimchiux123@gmail.com
    Co-authored-by: Tammy Le le.tammy979@gmail.com
    Co-authored-by: Lucas Mobley lucas@lucasmobley.com

---
+++ server/controllers/securityTestControllers.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import fetch from "node-fetch";
+
+const securityTestController = {
+  dos: ((req: Request, res: Response, next: any) => {
+    
+    //nested query which client should reject.
+    const queryTester = `
+    query {
+      Country{
+        borders{
+          officialLanguages{
+            countries{
+              borders{
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+    `;
+    
+    //fetch options
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: queryTester })
+    }
+    
+    fetch('https://countries-274616.ew.r.appspot.com//', options)
+    .then(result => result.text())
+    .then(result => {
+      res.locals.dos = result
+      return next();
+    })
+    .catch(err => {
+      next({
+        log: 'Express error handler caught responseTime middleware error',
+        message: {err: 'Can\'t retrieve response time'}
+      });
+    });
+  }),
+};
+
+export default securityTestController;
--- server/routes/api.ts
+++ server/routes/api.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, Router } from "express";
 import performanceTestControllers from "../controllers/performanceTestControllers";
+import securityTestController from "../controllers/securityTestControllers";
 
 const router = express.Router();
 
@@ -12,5 +13,11 @@ router.get('/',
     return res.status(200).json(res.locals.responseTime)
   }
 )
+
+router.get('/dos', 
+securityTestController.dos, 
+(req, res) => {
+  return res.status(200).json(res.locals.dos);
+})
 
 export default router;
