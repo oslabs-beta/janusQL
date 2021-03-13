589f2f8842faaae16a57d6340ad3c73e564f6c7d
letammy979 <le.tammy979@gmail.com>
2021-03-13 18:08:10 -0500

    cleaned up addUser controller fx
    Co-authored-by: Adrian Inza-Cruz ainzacruz@gmail.com
    Co-authored-by: Kim Chiu kimchiux123@gmail.com
    Co-authored-by: Phillip Bannister phillip.kbannister@gmail.com
    Co-authored-by: Lucas Mobley lucas@lucasmobley.com

--- server/controllers/userControllers.ts
+++ server/controllers/userControllers.ts
@@ -9,15 +9,11 @@ const userControllers = {
     const { username, fullname, password, email } = req.body;
     console.log('req body', req.body);
 
-    // store username, password, fullname, email into an array for the db query later
+    // store required fields into an array for the db query later
     const params = [username, fullname, password, email];
-    console.log('params', params);
 
-    // SCRUB USER INPUTS
-
-    // ensure inputs are valid
+    // ensure all required fields exist
     if (!username || !fullname || !password || !email) {
-      console.log('missing items err');
       return next({
         log: 'Missing username, fullname, password, and/or email in the userControllers.addUser middleware',
         message: {
@@ -25,8 +21,6 @@ const userControllers = {
         }
       });
     }
-    // inputs exist, inputs are strings, inputs don't have script tags
-    // trim the whitespace -> have questions LMAO
 
     // create a SQL query string
     const queryString = 'INSERT INTO users_table (username, fullname, "password", email) VALUES ($1, $2, $3, $4)';
@@ -37,7 +31,6 @@ const userControllers = {
         return next();
       })
       .catch((err) => {
-        // console.log(err)
         return next({
           log: 'error inserting username, fullname, password, email',
           message: {
