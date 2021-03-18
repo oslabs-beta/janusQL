981bb7b4573826a264e9f3726dc8547eb2db55e7
Kim Chiu <kimchiux123@gmail.com>
2021-03-18 11:23:39 -0700

    removed unnecessary comments in jest config page
    Co-authored-by: Adrian Inza-Cruz ainzacruz@gmail.com
    quote> Co-authored-by: Phillip Bannister phillip.kbannister@gmail.com
    quote> Co-authored-by: Tammy Le le.tammy979@gmail.com
    quote> Co-authored-by: Lucas Mobley lucas@lucasmobley.com

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,5 @@
 import type {Config} from '@jest/types';
 
-// sync object
-// const config: Config.InitialOptions = {
-//   verbose: true,
-// };
-// export default config;
-
 // async function
 export default async (): Promise<Config.InitialOptions> => {
   return {
